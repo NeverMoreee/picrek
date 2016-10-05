145e02c6c8a08c3afc8ec2e5a813dbf92bbc2e0a
nemos <593393454@qq.co>
2016-10-05 15:08:25 +0800

    tested the test.py

--- picrek/test.py
+++ picrek/test.py
@@ -23,29 +23,31 @@ class YPic(Pic):
 
 class UrlPar(object):
     def __init__(self, url):
-        self.pics = UrlPar.pics_par(UrlPar.html_par(UrlPar.get_html(url)))
+        self.pics = self.pics_par(self.html_par(self.get_html(url)))
 
     def __str__(self):
+        '''
         if self.pics:
             return 'empty'
+        '''
         str_ = ''
         for pic in self.pics:
-            str_ += (str(pic) + '\n')
+            str_ += str(pic) + '\n'
         return str_
 
-    def get_html(url):
+    def get_html(self, url):
         req = requests.get(url)
         return req.text
 
-    def html_par(html):
+    def html_par(self, html):
         return etree.HTML(html)
 
-    def pics_par(parsered):
+    def pics_par(self, parsered):
         pics = []
         a_eles = parsered.xpath('//a[@class="thumb"]')
         for a_ele in a_eles:
             matched = a_ele.xpath('@href| img/@src| img/@title')
-            matched[0] = matched[0].split('/')[4]
+            matched[0] = matched[0].split('/')[3]
             tags = []
             for x in matched[2].split(' '):
                 if x in ('Rating:', 'Score:', 'Tags:'):
@@ -58,4 +60,4 @@ class UrlPar(object):
 
 
 url = UrlPar('https://yande.re/post?tags=rating%3Asafe')
-print(url)
+print (url)
